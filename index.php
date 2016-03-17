368defd9d66a94c2b271e3acda03d90f02924999
Michel Frechette <mifrec@hotmail.com>
2016-03-17 12:49:25 -0400

    php creating, concatenating, echoing variables

--- index.php
+++ index.php
@@ -2,21 +2,46 @@
 <html>
 <body>
 
+<!-- // creating variables -->
 <?php
-// <?php PHP code goes here
-?>
-<br>
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+echo $txt;
+?><br>
 
-<h1>My first PHP page</h1>
+<!-- printing variables to the screen -->
+<?php
+$txt = "W3Schools.com";
+echo "<p>I love $txt!</p><br>";
+echo "<h1>I love $txt!</h1><br>";
+?><br>
+
+<!-- concatenation -->
+<?php
+$txt = "coding";
+echo "I love " . $txt . "!<br>";
+echo "<h2>I love " . $txt . "!</h2><br>";
+?>
+
+<!-- some of two variables -->
+<?php
+$x = 5;
+$y = 4;
+$z = $x + $y;
+echo $x + $y . "<br>";
+echo "The sum of " . $x . " and " . $y . " is: " . $z;
+?>
+
+<!-- <h1>My first PHP page</h1>
 
 <?php
 echo "Hello World!";
-?>
-<br>
+?><br>
 
 <?php
 echo "My first PHP script!";
-?>
+?><br> -->
 
 </body>
 </html>
