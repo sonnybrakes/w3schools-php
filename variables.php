de5c3603539f004e0e6b751d902df9509a778dc8
Michel Frechette <mifrec@hotmail.com>
2016-03-18 09:04:58 -0400

    Output Variables Examples

--- variables.php
+++ variables.php
@@ -2,19 +2,24 @@
 <html>
 <body>
 
-<h3>Creating (Declaring) PHP Variables</h3>
-
-<?php
-$txt = "Hello world!";
-$x = 5;
-$y = 10.5;
-?>
-
 <h3>Output Variables</h3>
-
+<h4>Example: 1, Echo</h4>
 <?php
 $txt = "W3Schools.com";
 echo "I love $txt!";
+?>
+
+<h4>Example: 2, Concatenation</h4>
+<?php
+$txt = "W3Schools.com";
+echo "I love " . $txt . "!";
+?>
+
+<h4>Example: 3, Sum of two variables</h4>
+<?php
+$x = 5;
+$y = 4;
+echo $x + $y;
 ?>
 
 </body>
